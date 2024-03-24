9cc4f035ea4c88498a78a6072cacd5b0abb52559
Mukesh Dasriya <dasriyamukesh87@gmail.com>
2024-03-24 13:03:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+import { CarouselCustomNavigation } from './CarouselCustomNavigation';
+import { NavbarWithMegaMenu } from './component/Navbar';
+function App() {
+ 
+
+  return (
+    <>
+    <NavbarWithMegaMenu></NavbarWithMegaMenu>
+   <CarouselCustomNavigation />
+    </>
+  )
+}
+
+export default App
